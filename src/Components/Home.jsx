cab262ad22a4f9ef64a191f94bb084c72842dfa2
mahdi056 <ahnafmahdi12@gmail.com>
2025-01-15 10:24:03 +0600

    banner setup

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,49 @@
-
+import img1 from '../assets/img1.avif';
+import img2 from '../assets/img2.jpeg';
+import img3 from '../assets/img3.jpeg';
 
 const Home = () => {
     return (
         <div>
-            
-            
+            {/* banner */}
+
+            <div className='flex justify-center'>
+
+            <div className="carousel w-4/5">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src={img1}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src={img2}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src={img3}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+               
+             
+            </div>
+
+
+            </div>
+           
         </div>
     );
 };
